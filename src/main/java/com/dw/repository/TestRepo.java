1b4fe4396110b32565257c2bd2c3fb68fccb79e9
ahmad adawi <ahmad.adawi@intellisc.com>
2019-07-13 18:12:54 +0300

    clean up code

--- src/main/java/com/dw/repository/TestRepo.java
+++
@@ -1,9 +0,0 @@
-package com.dw.repository;
-
-import com.dw.model.Test;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TestRepo extends MongoRepository<Test,String> {
-
-    Test findByName(String name);
-}
